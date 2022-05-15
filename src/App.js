3a5bbf2dc771bde2326f60619a42b43ba3cda6f7
anshumandixit9 <anshumandixit7@gmail.com>
2022-05-15 14:49:59 +0530

    Added Navbar[+Anshuman]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import React from "react";
+import Home from "./components/home-components/Home";
 function App() {
   return (
-    <div>
-      <h1> Initial Setup </h1>
-    </div>
+    <Home/>
   );
 }
 
---
+++ src/components/home-components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./nav-bar";
+function Home(){
+    console.log("Hello I'm Working!");
+    return(
+        <div>
+            <Nav/>
+        </div>
+    );
+}
+export default Home;
